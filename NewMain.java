2946b9d2e828342f63cb1f35434637a422491c58
gloger85 <glogo85@gmail.com>
2016-08-10 23:21:45 +0200

    Add files via upload

---
+++ NewMain.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author glogo
+ */
+public class NewMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("buenos dias world!");
+    }
+    
+}
